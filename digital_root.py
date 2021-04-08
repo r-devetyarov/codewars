03227e6a6155608498ac29725ea130f54030e8f2
Renat <rdevetiarov@neuro.net>
2021-04-08 10:25:07 +0300

    Add digital_root

---
+++ digital_root.py
@@ -0,0 +1,28 @@
+""""
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit,
+ continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1
+"""
+
+
+def digital_root(n: int) -> int:
+    new_n = 0
+    for element in str(n):
+        new_n += int(element)
+    if len(str(new_n)) > 1:
+        new_n = digital_root(new_n)
+    return new_n
+
+
+if __name__ == '__main__':
+    print(digital_root(16))
+    print(digital_root(942))
+    print(digital_root(132189))
+    print(digital_root(493193))
